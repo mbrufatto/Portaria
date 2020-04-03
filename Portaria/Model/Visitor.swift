085ecf2f06222e7c67b052bb0b079508b26fafe5
Márcio Habigzang Brufatto <mhbrufatto@gmail.com>
2020-04-02 23:52:35 -0300

    Criada o modelo do visitante.

---
+++ Portaria/Model/Visitor.swift
@@ -0,0 +1,16 @@
+//
+//  Visitor.swift
+//  Portaria
+//
+//  Created by Marcio Habigzang Brufatto on 02/04/20.
+//  Copyright © 2020 Marcio Habigzang Brufatto. All rights reserved.
+//
+
+import Foundation
+
+struct Visitor {
+    let endDateAndHour: Date?
+    let name: String
+    let phone: String
+    let responsibleAccess: String
+}

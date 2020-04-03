db579002d7b8156bbd5fa342cb0621dfb86a9cbe
Márcio Habigzang Brufatto <mhbrufatto@gmail.com>
2020-04-03 01:05:18 -0300

    Criada a camada de negócio

---
+++ Portaria/Bussiness/VisitorProtocol.swift
@@ -0,0 +1,17 @@
+//
+//  VisitorProtocol.swift
+//  Portaria
+//
+//  Created by Marcio Habigzang Brufatto on 03/04/20.
+//  Copyright © 2020 Marcio Habigzang Brufatto. All rights reserved.
+//
+
+import Foundation
+
+protocol VisitorProtocol {
+    func addVisitorInContact(visitor: Visitor)
+    func addVisitorInDatabase(visitor: Visitor)
+    func filterByWeek()
+    func filterByMonth()
+    func filterByYear()
+}
